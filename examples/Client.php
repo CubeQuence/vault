5fa5a1afc528b3b2f03ecce5789460f65089be8a
Luca Castelnuovo <luca@castelnuovo.xyz>
2021-10-18 13:21:14 +0200

    Rename vault example

--- examples/Client.php
+++ examples/Client.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+require __DIR__ . '/../vendor/autoload.php';
+
 // https://www.vaultproject.io/api-docs/auth
 // https://www.vaultproject.io/api-docs/secret
 
@@ -10,11 +12,11 @@ use CQ\Vault\Auth\Provider\Token;
 use CQ\Vault\Client;
 
 try {
-    $tokenProvider = new Token(token: 's.XXXXXXXXXXXX');
-    $approleProvider = new AppRole(
-        roleId: 'XXXXXX-XXXXXX-XXXXXX-XXXXXX',
-        secretId: 'XXXXXX-XXXXXX-XXXXXX-XXXXXX'
-    );
+    $tokenProvider = new Token(token: 's.1mfDZPS7t95g1ZubOdMuYcOJ');
+    // $approleProvider = new AppRole(
+    //     roleId: 'XXXXXX-XXXXXX-XXXXXX-XXXXXX',
+    //     secretId: 'XXXXXX-XXXXXX-XXXXXX-XXXXXX'
+    // );
 
     $client = new Client(
         authProvider: $tokenProvider,
@@ -22,12 +24,12 @@ try {
         version: 'v1' // Optional variable
     );
 
-    $write = $client->write('/kv1/helloworld', [
+    $write = $client->post('/kv1/helloworld', [
         'foo' => 'bar',
     ]);
-    $read = $client->read('/kv1/helloworld');
-    $keys = $client->keys('/kv1');
-    $revoke = $client->revoke('/kv1/helloworld');
+    $read = $client->get('/kv1/helloworld');
+    $keys = $client->list('/kv1');
+    $revoke = $client->delete('/kv1/helloworld');
 } catch (\Throwable $th) {
     echo $th->getMessage();
     exit;
